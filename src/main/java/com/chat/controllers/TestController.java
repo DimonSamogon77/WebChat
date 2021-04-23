209bd56ee34897e098d4a0c3a5694112a01b10df
DimonSamogon77 <zyzychka1@gmail.com>
2021-04-23 14:40:01 +0300

    test

---
+++ src/main/java/com/chat/controllers/TestController.java
@@ -0,0 +1,21 @@
+package com.chat.controllers;
+
+import com.chat.models.Person;
+import com.chat.models.TextMessage;
+import lombok.SneakyThrows;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class TestController {
+
+    @SneakyThrows
+    @MessageMapping("/hello")
+    @SendTo("/topic/chat")
+    public TextMessage hi(Person person){
+        Thread.sleep(1000);
+        return new TextMessage(HtmlUtils.htmlEscape("Hello "+person.getName()));
+    }
+}
---
+++ src/main/java/com/chat/models/TextMessage.java
@@ -0,0 +1,12 @@
+package com.chat.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class TextMessage {
+    private String content;
+}
